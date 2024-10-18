aa12be67206c30088cc5ad2144d34d24d17a4e5c
SONG HAHYEON <164151175+hahyeon01@users.noreply.github.com>
2024-10-18 21:11:05 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 # 양수와 음수의 개수를 세는 프로그램
 
-# n개의 정수를 입력받기
-n = int(input("Enter the number of integers: "))
+# a개의 정수를 입력받기
+a = int(input("Enter the number of integers: "))
 
 # 양수와 음수의 개수를 저장할 변수
 positive_count = 0
 negative_count = 0
 
-# n개의 정수를 입력받아 양수인지 음수인지 판별
-for i in range(n):
+# a개의 정수를 입력받아 양수인지 음수인지 판별
+for i in range(a):
     x = int(input(f"Enter integer {i + 1}: "))
 
     if x < 0:
